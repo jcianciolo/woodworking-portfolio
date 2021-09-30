1f77d7126537cdd3a4c77df475e24761d1c2fd42
DESKTOP-HOSGHCR\johnc <jrcianc@gmail.com>
2021-09-29 21:49:43 -0700

    test

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import Typography from '@mui/material/Typography'
+import Grid from '@mui/material/Grid'
 
 const About = () => {
     return (
         <>
             <Typography variant="h2" sx={{ margin: 5 }}>About the artist</Typography>
             
-            <div sx={{  }}>
-                <img src='https://picsum.photos/300/400' alt="alex working on a table" />
-                <Typography variant="subtitle1" sx={{ margin: 5 }}>Lorem ipsum etc...</Typography>
-            </div>
+            <Grid container sx={{ justifyContent: 'space-evenly', alignItems: 'center' }}>
+                <Grid item xs={12} sm={8} md={5} >
+                    <img src='https://picsum.photos/400/300' style={{borderRadius: '1rem'}} alt="alex working on a table" />
+                </Grid>
+                <Grid item xs={12} sm={8} md={5} >
+                    <Typography variant="h5" sx={{ margin: 0 }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus totam nihil pariatur a possimus fugit maxime aliquid obcaecati aliquam nam minima, ullam magni voluptate explicabo, id accusantium. Nihil, reprehenderit iste!</Typography>
+                </Grid>
+            </Grid>
             
         </>
     )
---
+++ src/components/MasonryImageList.js
@@ -0,0 +1,108 @@
+import * as React from 'react';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import { CardActionArea } from '@mui/material';
+import Typography from '@mui/material/Typography'
+import { useState, useEffect } from 'react';
+import Masonry from '@mui/lab/Masonry';
+import MasonryItem from '@mui/lab/MasonryItem';
+
+
+export default function MasonryImageList() {
+
+  return (
+    <Masonry sx={{ flexGrow: 1 }} className="image-list" cols={4} gap={4}>
+      {itemData && itemData.map((item) => (
+        <Card>
+          <CardActionArea>
+            <CardMedia>
+              <MasonryItem key={item.id}>
+                <img
+                  src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+                  srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                  alt={item.author}
+                  loading="lazy"
+                />
+                <ImageListItemBar
+                  title={item.title}
+                  subtitle={<span>by: {item.author}</span>}
+                  position="top"
+                />
+              </MasonryItem>
+            </CardMedia>
+          </CardActionArea>
+        </Card>
+
+      ))}
+    </Masonry>
+  );
+}
+
+const itemData = [
+  {
+    img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
+    title: 'Breakfast',
+    author: '@bkristastucchio',
+    featured: true,
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
+    title: 'Burger',
+    author: '@rollelflex_graphy726',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
+    title: 'Camera',
+    author: '@helloimnik',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
+    title: 'Coffee',
+    author: '@nolanissac',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
+    title: 'Hats',
+    author: '@hjrc33',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
+    title: 'Honey',
+    author: '@arwinneil',
+    featured: true,
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
+    title: 'Basketball',
+    author: '@tjdragotta',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
+    title: 'Fern',
+    author: '@katie_wasserman',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
+    title: 'Mushrooms',
+    author: '@silverdalex',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
+    title: 'Tomato basil',
+    author: '@shelleypauls',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
+    title: 'Sea star',
+    author: '@peterlaster',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
+    title: 'Bike',
+    author: '@southside_customs',
+  },
+];
