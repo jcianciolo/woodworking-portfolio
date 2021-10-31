c75efbf8f9cd69aad1455932f7c6d00ef86f002f
DESKTOP-HOSGHCR\johnc <jrcianc@gmail.com>
2021-10-31 00:42:53 -0700

    set up routing, cleaned up some stuff

--- src/App.js
+++ src/App.js
@@ -4,31 +4,64 @@ import Gallery from './components/Gallery';
 import About from './components/About';
 import Contact from './components/Contact';
 import { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   const [gallery, setGallery] = useState(true)
   const [aboutMe, setAboutMe] = useState(false)
   const [contact, setContact] = useState(false)
-
+  
   return (
-    <div className="App">
-      <ButtonAppBar
-        gallery={gallery}
-        setGallery={setGallery}
-        aboutMe={aboutMe}
-        setAboutMe={setAboutMe}
-        contact={contact}
-        setContact={setContact}
-      />
-      {gallery && <Gallery />}
+    <Router>
+      <div className="App">
+      <ButtonAppBar />
 
-      {aboutMe && <About />}
-      {contact && <Contact />}
 
-    </div>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/">
+            <Gallery />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
 export default App;
 // <CustomImageList />
 //      
+
+// function App() {
+
+
+//   return (
+//     <div className="App">
+//       <ButtonAppBar
+//         gallery={gallery}
+//         setGallery={setGallery}
+//         aboutMe={aboutMe}
+//         setAboutMe={setAboutMe}
+//         contact={contact}
+//         setContact={setContact}
+//       />
+//       {gallery && <Gallery />}
+
+//       {aboutMe && <About />}
+//       {contact && <Contact />}
+
+//     </div>
+//   );
+// }
--- src/components/ButtonAppBar.js
+++ src/components/ButtonAppBar.js
@@ -5,28 +5,9 @@ import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import Avatar from '@mui/material/Avatar';
+import { Link } from "react-router-dom";
 
-export default function ButtonAppBar({ setGallery, setAboutMe, setContact }) {
-  const handleClick = (e) => {
-    if (e.target.value === 'Gallery') {
-        setGallery(true)
-        setAboutMe(false)
-        setContact(false)
-    }
-
-    if (e.target.value === 'About Me') {
-      setAboutMe(true)
-      setGallery(false)
-      setContact(false)
-    }
-
-    if (e.target.value === 'Contact') {
-      setContact(true)
-      setGallery(false)
-      setAboutMe(false)
-    
-}
-  }
+export default function ButtonAppBar() {
 
   return (
     <Box sx={{ flexGrow: 1 }}>
@@ -34,24 +15,33 @@ export default function ButtonAppBar({ setGallery, setAboutMe, setContact }) {
         <Toolbar sx={{ justifyContent:"space-between" }}>
 
           <Avatar alt='ajk' src='https://images.squarespace-cdn.com/content/v1/559c3ccce4b0cebfa4ebbf72/1436844658707-OQ5WMM3I9X4KBPCFISFF/simple+logo+art+and+wood.jpg' />
-          <ButtonGroup onClick={handleClick} color="inherit" aria-label="text button group" sx={{ display: { xs: 'none', lg: 'flex' } }}>
-            <Button value='Gallery'>Gallery</Button>
-            <Button value='About Me'>About Me</Button>
-            <Button value='Contact'>Contact</Button>
+          <ButtonGroup color="inherit" aria-label="text button group" sx={{ display: { xs: 'none', lg: 'flex' } }}>
+            <Button value='Gallery'><Link className="link" to="/">Gallery</Link></Button>
+            <Button value='About Me'><Link className="link" to="/about">About Me</Link></Button>
+            <Button value='Contact'><Link className="link" to="/contact">Contact</Link></Button>
           </ButtonGroup>
         </Toolbar>
       </AppBar>
     </Box>
   );
 }
+//   const handleClick = (e) => {
+//     if (e.target.value === 'Gallery') {
+//         setGallery(true)
+//         setAboutMe(false)
+//         setContact(false)
+//     }
 
-// <IconButton
-// size="large"
-// edge="start"
-// color="inherit"
-// aria-label="menu"
-// sx={{ mr: 2 }}
-// >
-// <MenuIcon />
+//     if (e.target.value === 'About Me') {
+//       setAboutMe(true)
+//       setGallery(false)
+//       setContact(false)
+//     }
 
-// </IconButton>
+//     if (e.target.value === 'Contact') {
+//       setContact(true)
+//       setGallery(false)
+//       setAboutMe(false)
+    
+// }
+//   }
